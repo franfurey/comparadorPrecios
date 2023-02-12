2974c50d106a0bfb64fa8887adbd1a1781bbeaf4
Francisco Furey <franciscofurey@gmail.com>
2023-02-11 22:05:04 -0600

    Dejé de seguir rastreando archivos

--- pages/credentials.py
+++
@@ -1,9 +0,0 @@
-email_furey = 'info@upalalaweb.com.ar'
-password_furey = 'upa1174'
-
-
-email_canal = 'franciscofurey@gmail.com'
-password_furey = 'franciscofurey@gmail.com'
-
-email_mg = 'info@upalalaweb.com'
-password_mg = 'upa123'
